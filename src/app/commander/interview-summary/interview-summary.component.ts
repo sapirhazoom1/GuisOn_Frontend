33d5c2b2f926d806f22b0e6b9f6c569c57c12c8c
andrey <andreosx5000@gmail.com>
2025-02-06 20:20:47 +0200

    Update files

--- src/app/commander/interview-summary/interview-summary.component.ts
+++ src/app/commander/interview-summary/interview-summary.component.ts
@@ -193,7 +193,7 @@ export class InterviewSummaryComponent implements OnInit, OnDestroy {
       });
     }
   }
-
+// adasdads
   onCancel(): void {
     if (this.isInterviewScheduled) {
       this.interviewNotes = this.originalInterviewData.interviewNotes || '';
